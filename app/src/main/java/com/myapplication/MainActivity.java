9924cd0c4887e8a6360212b6060c63555626d80e
SoGho2580 <soumoghosh37@gmail.com>
2021-05-11 13:21:54 +0530

    Update README.md

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -9,10 +9,11 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
-
-import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -35,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar1 = findViewById(R.id.toolbar1);
         LinearLayout linear2 = findViewById(R.id.linear2);
-        AppUpdater appUpdater = new AppUpdater(this);
         button4 = findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,9 +61,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateCheck() {
         AppUpdater appUpdater = new AppUpdater(this);
-        appUpdater.start();
+        appUpdater.setUpdateFrom(UpdateFrom.XML);
+        appUpdater.setUpdateXML("https://raw.githubusercontent.com/SoGho2580/AppUpdater/master/app/update.xml");
         appUpdater.setDisplay(Display.DIALOG).setCancelable(true);
-        appUpdater.setDisplay(Display.NOTIFICATION);
-        appUpdater.setUpdateFrom(UpdateFrom.XML).setUpdateXML("https://github.com/SoGho2580/AppUpdater/raw/master/app/update.xml");
+        appUpdater.setTitleOnUpdateAvailable("Update available")
+                .setContentOnUpdateAvailable("Check out the latest version available of my app!")
+                .setTitleOnUpdateNotAvailable("Update not available")
+                .setContentOnUpdateNotAvailable("No update available. Check for updates again later!")
+                .setButtonUpdate("Update now?");
+                //.setButtonUpdateClickListener(...)
+	appUpdater.setButtonDismiss("Maybe later");
+                //.setButtonDismissClickListener(...)
+        appUpdater.showAppUpdated(true);
+        appUpdater.start();
     }
 }
