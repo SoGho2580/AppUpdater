017b1ed96aa5379b605c2834f22e58b1e560e09a
SoGho2580 <soumoghosh37@gmail.com>
2021-05-11 12:44:23 +0530

    Update README.md

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -22,41 +22,48 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar1;
     private Button button2;
     private LinearLayout linear2;
-    private Intent i=new Intent();
+    private Intent i = new Intent();
     private FirebaseUser user;
-    private TextView textview5;
+    private Button button4;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user= mAuth.getCurrentUser();
+        FirebaseUser user = mAuth.getCurrentUser();
         setContentView(R.layout.activity_main);
-        Toolbar toolbar1=findViewById(R.id.toolbar1);
-        LinearLayout linear2=findViewById(R.id.linear2);
-        textview5=findViewById(R.id.textview5);
+        Toolbar toolbar1 = findViewById(R.id.toolbar1);
+        LinearLayout linear2 = findViewById(R.id.linear2);
         AppUpdater appUpdater = new AppUpdater(this);
-        appUpdater.start();
-        appUpdater.setDisplay(Display.DIALOG).setCancelable(false);
-        appUpdater.setDisplay(Display.NOTIFICATION);
-        appUpdater.setUpdateFrom(UpdateFrom.XML).setUpdateXML("https://github.com/SoGho2580/AppUpdater/raw/master/app/update.xml");
-        Button button2=findViewById(R.id.button2);
-
-        textview5.setText(user.getEmail());
-         button2.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 FirebaseAuth.getInstance().signOut();
-                 finish();
-             }
-         });
-         toolbar1.setNavigationOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 onBackPressed();
-             }
-         });
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateCheck();
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
+        toolbar1.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
+    private void updateCheck() {
+        AppUpdater appUpdater = new AppUpdater(this);
+        appUpdater.start();
+        appUpdater.setDisplay(Display.DIALOG).setCancelable(true);
+        appUpdater.setDisplay(Display.NOTIFICATION);
+        appUpdater.setUpdateFrom(UpdateFrom.XML).setUpdateXML("https://github.com/SoGho2580/AppUpdater/raw/master/app/update.xml");
+    }
 }
