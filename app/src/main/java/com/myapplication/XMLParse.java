f468b68fe931eea3287009330ae64c00d0d0476b
SoGho2580 <soumoghosh37@gmail.com>
2021-05-13 13:36:39 +0530

    nothin

---
+++ app/src/main/java/com/myapplication/XMLParse.java
@@ -0,0 +1,13 @@
+package com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class XMLParse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
